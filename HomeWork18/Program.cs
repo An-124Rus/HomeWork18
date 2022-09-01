5cff40c23f08d530571319c136dd2c0aad337516
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-09-01 16:16:22 +0300

    HomeWork18

---
+++ HomeWork18/Program.cs
@@ -0,0 +1,45 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int count = 0;
+        int deep = 0;
+
+        Console.Write("Введите строку с символами '(' и ')': ");
+        string brackets = Console.ReadLine();
+
+        for (int i = 0; i < brackets.Length; i++)
+        {
+            if (brackets[i] == '(')
+            {
+                count++;
+            }
+
+            if (brackets[i] == ')')
+            {
+                if(deep < count)
+                {
+                    deep = count;
+                }
+
+                count--;
+            }
+
+            if(count < 0)
+            {
+                break;
+            }
+        }
+
+        if (count == 0)
+        {
+            Console.WriteLine($"Корректное скобочное выражение, максимальная глубина вложенности скобок {deep}.");
+        }
+        else
+        {
+            Console.WriteLine("Не корректное скобочное выражение");
+        }
+    }
+}
